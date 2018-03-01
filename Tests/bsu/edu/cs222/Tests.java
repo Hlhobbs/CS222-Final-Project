ea0b8267f36919b459d7dad9819fa51ef167be19
Hayden Hobbs <haydenleehobbs@gmail.com>
2018-03-01 10:42:58 -0500

    Writing first test, if it works it should show an image.

--- Tests/bsu/edu/cs222/Tests.java
+++ Tests/bsu/edu/cs222/Tests.java
@@ -1,4 +1,23 @@
 package bsu.edu.cs222;
 
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Tests {
+
+    @Test
+    public void PictureTest(){
+        BufferedImage picture = null;
+        try{
+            picture = ImageIO.read(new File("/Users/HaydenHobbs/Desktop/L.jpg"));
+        } catch (IOException e){
+
+        }
+        
+    }
 }
