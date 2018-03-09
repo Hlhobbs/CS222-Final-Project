4858c412e42be8de0c9968a0db7550ee3b780581
Ryan Ahler <rahler@bsu.edu?>
2018-03-09 10:32:57 -0500

    Expanded upon ScanPictureForColors.java to allow for JavaFX image objects (still untested) and began writing a test.

--- src/edu/bsu/cs222/ScanPictureForColors.java
+++ src/edu/bsu/cs222/ScanPictureForColors.java
@@ -1,5 +1,8 @@
 package edu.bsu.cs222;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
@@ -35,6 +38,32 @@ public class ScanPictureForColors {
         return listofscannedpixels;
     }
 
+    public List<Pixel> ScanPixelsforColors(Image image) {
 
+        List<Pixel> listofscannedpixels = new ArrayList<>();
 
+        double w = image.getWidth();
+        double h = image.getHeight();
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+
+                Pixel datapixel = new Pixel();
+                datapixel.setxCoordinate(j);
+                datapixel.setyCoordinate(i);
+
+                PixelReader pixelReader =image.getPixelReader();
+                int picturepixel = pixelReader.getArgb(j,i);
+
+                datapixel.setAlphaValue( (picturepixel >> 24) & 0xff);
+                datapixel.setRedValue( (picturepixel >> 16) & 0xff );
+                datapixel.setGreenValue( (picturepixel >> 8) & 0xff);
+                datapixel.setBlueValue( (picturepixel) & 0xff);
+                listofscannedpixels.add(datapixel);
+                ;
+
+            }
+        }
+        return listofscannedpixels;
+    }
 }
---
+++ tests/edu/bsu/cs222/scanTests.java
@@ -0,0 +1,47 @@
+package edu.bsu.cs222;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.List;
+
+public class scanTests extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+
+        FileChooser testFileChooser = new FileChooser();
+        testFileChooser.setTitle("Test File Chooser");
+        testFileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif")
+        );
+
+        File chosenTestFile = testFileChooser.showOpenDialog(null);
+
+        InputStream inputStream = new FileInputStream(chosenTestFile);
+        createImage image = new createImage(inputStream);
+        Image createdImage = image.returnImage();
+        javafx.scene.image.ImageView imageView = new javafx.scene.image.ImageView(createdImage);
+
+        StackPane Pane = new StackPane();
+        Pane.getChildren().add(imageView);
+
+        Scene scene = new Scene(Pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        ScanPictureForColors scanPictureForColors = new ScanPictureForColors();
+        List<Pixel> list = scanPictureForColors.ScanPixelsforColors(createdImage);
+
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
