b35d6ddde681ff9ab24d62c503681c83bcc27368
Matthew Taylor <mktaylor3>
2018-03-09 13:25:26 -0500

    removed buffered image method

--- src/edu/bsu/cs222/ScanPictureForColors.java
+++ src/edu/bsu/cs222/ScanPictureForColors.java
@@ -2,41 +2,12 @@ package edu.bsu.cs222;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.PixelReader;
-
-import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ScanPictureForColors {
 
 
-    public List<Pixel> ScanPixelsforColors(BufferedImage image) {
-
-        List<Pixel> listofscannedpixels = new ArrayList<>();
-
-        int w = image.getWidth();
-        int h = image.getHeight();
-
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-
-                Pixel datapixel = new Pixel();
-                datapixel.setxCoordinate(j);
-                datapixel.setyCoordinate(i);
-
-                int picturepixel = image.getRGB(j, i);
-
-                datapixel.setAlphaValue( (picturepixel >> 24) & 0xff);
-                datapixel.setRedValue( (picturepixel >> 16) & 0xff );
-                datapixel.setGreenValue( (picturepixel >> 8) & 0xff);
-                datapixel.setBlueValue( (picturepixel) & 0xff);
-                listofscannedpixels.add(datapixel);
-                ;
-
-            }
-        }
-        return listofscannedpixels;
-    }
 
     public List<Pixel> ScanPixelsforColors(Image image) {
 
