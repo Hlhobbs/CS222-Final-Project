ad9c2058fbc71428de31340531d4d3efc9c42427
aluees <rcklueber@bsu.edu>
2018-03-10 14:59:12 -0500

    Broken/Work in progress Controller class with table Class. Specifically needs ObservableList for table data

---
+++ src/edu/bsu/cs222/mainController.java
@@ -0,0 +1,34 @@
+package edu.bsu.cs222;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+public class mainController {
+
+    TableView<Pixel> table;
+
+    public void start(Stage primaryStage) {
+        //Color Column
+        TableColumn<Pixel, Pixel> colorColumn = new TableColumn<>("Color");
+        colorColumn.setCellValueFactory(new PropertyValueFactory<>("color"));
+
+        //Hex Column
+        TableColumn<Pixel, Pixel> hexColumn = new TableColumn<>("Hex Value");
+        hexColumn.setCellValueFactory(new PropertyValueFactory<>("hex"));
+
+        //Sets up Table
+        table = new TableView<>();
+        //Needs ObservableList with data to populate aka getData()
+        //table.setItems(ObservableList);
+        table.getColumns().addAll(colorColumn, hexColumn);
+    }
+
+    //public ObservableList<type> getData(){
+        //ObservableList<type> datas = FXCollections.observableArrayList();
+        //datas.add(new data());
+        //return datas;
+}
