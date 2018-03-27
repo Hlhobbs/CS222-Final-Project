337aa0acea8fb8614be4b734c16884490d0ca4bb
Ryan Ahler <rahler@bsu.edu?>
2018-03-27 12:27:33 -0400

    Cleaned up import statements

--- src/edu/bsu/cs222/SimplifyColors.java
+++ src/edu/bsu/cs222/SimplifyColors.java
@@ -4,23 +4,25 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class SimplifyColors {
 
     private int count = 0;
+    private ObservableList<String> observableList;
 
     public ObservableList<String> DeleteRepeatColors(ObservableList<String> list) {
+        this.observableList = list;
         ObservableList<String> returnList = FXCollections.observableArrayList();
-        while (list.isEmpty() != true) {
+        while (observableList.isEmpty() != true) {
             String color = list.get(0);
             returnList.add(color);
-            list.removeAll(color);
+            observableList.removeAll(color);
             System.out.println(list.size());
         }
         return returnList;
     }
-
 
     //public ObservableList<Display> CombineSimilarColors(ObservableList<Display> list) {
     //}
