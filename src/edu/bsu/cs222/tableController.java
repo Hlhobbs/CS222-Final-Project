ccbbd9327f373ea2a2dcc9f974948c84338c4dc1
rcahler <rahler@bsu.edu>
2018-03-29 00:11:43 -0400

    Started turing tableView into FXML

--- src/edu/bsu/cs222/tableController.java
+++ src/edu/bsu/cs222/tableController.java
@@ -3,9 +3,11 @@ package edu.bsu.cs222;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -18,7 +20,7 @@ import javafx.util.Callback;
 
 import java.util.List;
 
-public class tableController extends Application {
+public class tableController  {
 
     private ObservableList<Display> displays;
 
@@ -26,13 +28,27 @@ public class tableController extends Application {
         this.displays = FXCollections.observableArrayList(DList);
     }
 
-    @Override
-    public void start(Stage primaryStage) {
+    @FXML
+    private HBox parent;
 
+    @FXML
+    private Label label;
+
+    @FXML
+    private TableView<Display> tableView;
+
+    @FXML
+    private TableColumn<Display, String> hexCol;
+    @FXML
+    private TableColumn<Display, String> thumbnailCol;
+    @FXML
+    private TableColumn<Display, String> countCol;
+
+        /**
         TableView<Display> tableView = new TableView<Display>(displays);
 
 
-        TableColumn<Display, String> hexCol = new TableColumn<>("hex");
+        TableColumn<Display, String> hexCol = new TableColumn<>("Hexadecimal color codes");
         hexCol.setCellValueFactory(new PropertyValueFactory<>("hex"));
         hexCol.setPrefWidth(120);
 
@@ -73,19 +89,12 @@ public class tableController extends Application {
         tableView.getColumns().addAll(hexCol,thumbnailCol,countCol);
 
         HBox parent = new HBox();
-        parent.setPrefSize(600, 400);
-        parent.setPadding(new Insets(5));
-        parent.setSpacing(5);
-        parent.setAlignment(Pos.CENTER);
-        VBox vBox = new VBox();
-        vBox.setPrefSize(parent.getPrefWidth(), parent.getPrefHeight());
 
-        vBox.getChildren().add(tableView);
-        parent.getChildren().addAll(vBox);
+        parent.getChildren().add(tableView);
 
         primaryStage.setScene(new Scene(parent));
         primaryStage.setTitle("List of Hexadecimal color codes");
-        primaryStage.show();
+        primaryStage.show();**/
 
     }
-}
+
