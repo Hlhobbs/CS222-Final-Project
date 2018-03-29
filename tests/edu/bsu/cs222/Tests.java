f41b22536b95358794bd3a37790c81d9e1d2cd95
aluees <rcklueber@bsu.edu>
2018-03-29 13:43:59 -0400

    SimplifyColors test added to Tests class.

--- tests/edu/bsu/cs222/Tests.java
+++ tests/edu/bsu/cs222/Tests.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class Tests {
 
     //When using the exampleImage, the LinkedList HexValues should contain the following values [#000000, #ffffff, #000000, #ffffff, #000000, #ffffff, #000000, #ffffff, #000000, #ffffff, #ffffff, #000000, #ffffff, #000000, #ffffff, #000000, #ffffff, #000000, #ffffff, #000000, #ff0000, #ffffff, #ff0000, #ffffff, #ff0000, #ffffff, #ff0000, #ffffff, #ff0000, #ffffff, #ffffff, #ff0000, #ffffff, #ff0000, #ffffff, #ff0000, #ffffff, #ff0000, #ffffff, #ff0000, #24ff00, #ffffff, #24ff00, #ffffff, #24ff00, #ffffff, #24ff00, #ffffff, #24ff00, #ffffff, #ffffff, #24ff00, #ffffff, #24ff00, #ffffff, #24ff00, #ffffff, #24ff00, #ffffff, #24ff00, #f0ff00, #ffffff, #f0ff00, #ffffff, #f0ff00, #ffffff, #f0ff00, #ffffff, #f0ff00, #ffffff, #ffffff, #f0ff00, #ffffff, #f0ff00, #ffffff, #f0ff00, #ffffff, #f0ff00, #ffffff, #f0ff00, #ff5b8d, #ffffff, #ff5b8d, #ffffff, #ff5b8d, #ffffff, #ff5b8d, #ffffff, #ff5b8d, #ffffff, #ffffff, #ff5b8d, #ffffff, #ff5b8d, #ffffff, #ff5b8d, #ffffff, #ff5b8d, #ffffff, #ff5b8d]
-    //When using the exampleImage, the LinkedList, ShrinkedHexValues should containt the following values [#000000, #ffffff, #ff0000, #24ff00, #f0ff00, #ff5b8d]
+    //When using the exampleImage, the LinkedList, ShrinkedHexValues should contain the following values [#000000, #ffffff, #ff0000, #24ff00, #f0ff00, #ff5b8d]
 
 
     @Test
@@ -32,7 +32,7 @@ public class Tests {
     @Test
     public void testReturnStringHexValue() {
         Pixel pixel = new Pixel();
-        returnStringHexValue hex = new returnStringHexValue();
+        ReturnStringHexValue hex = new ReturnStringHexValue();
         pixel.setBlueValue(255);
         pixel.setRedValue(255);
         pixel.setGreenValue(255);
