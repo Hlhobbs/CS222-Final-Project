934f93799ff4aef7214120525a3866b333f20b69
Ryan Ahler <rahler@bsu.edu?>
2018-04-12 10:14:28 -0400

    Started working on making the Thumbnails clickable in the tableView. Right now it only displays a phrase in the CL when clicked.

---
+++ src/edu/bsu/cs222/ThumbnailClickCellFactory.java
@@ -0,0 +1,46 @@
+package edu.bsu.cs222;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.util.Callback;
+
+
+public class ThumbnailClickCellFactory implements Callback<TableColumn, TableCell> {
+
+    private final EventHandler<Event> click;
+
+    public ThumbnailClickCellFactory(EventHandler click) {
+        this.click = click;
+    }
+
+    @Override
+    public TableCell call(TableColumn param) {
+        TableCell<Pixel, String> cell = new TableCell<>() {
+            public void updateItem(String s, boolean empty) {
+                if (s != null) {
+                    HBox box = new HBox();
+                    box.setSpacing(10);
+                    VBox vbox = new VBox();
+
+                    ImageView imageview = new ImageView();
+                    imageview.setFitHeight(50);
+                    imageview.setFitWidth(50);
+                    imageview.setImage(new ThumbnailFromHexValue(s).returnImage());
+
+                    box.getChildren().addAll(imageview, vbox);
+                    //SETTING ALL THE GRAPHICS COMPONENT FOR CELL
+                    setGraphic(box);
+                }
+            }
+        };
+        if (click != null) {
+            cell.setOnMouseClicked(click);
+        }
+        return cell;
+    }
+}
