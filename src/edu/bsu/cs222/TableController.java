8b13cf972a72e853bab820555a5885a7682a62ef
Ryan Ahler <rahler@bsu.edu?>
2018-04-24 11:09:20 -0400

    Triad tab now functions.

--- src/edu/bsu/cs222/TableController.java
+++ src/edu/bsu/cs222/TableController.java
@@ -5,12 +5,14 @@ import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
+import javax.swing.event.TreeModelEvent;
 import java.net.URL;
 import java.util.LinkedList;
 import java.util.ResourceBundle;
@@ -42,15 +44,38 @@ public class TableController implements Initializable {
     @FXML
     private
     Label totalDistinctColors;
+
+
     @FXML
     TabPane tabPane;
     @FXML
     private
     Tab compTab;
     @FXML
-    private VBox box;
+    private
+    Tab triadTab;
+    @FXML
+    private VBox compBox;
     @FXML
     private ImageView compView;
+
+    @FXML
+    private VBox triadBox;
+    @FXML
+    private ImageView triadView_1;
+    @FXML
+    private ImageView triadView_2;
+    @FXML
+    private ImageView triadView_3;
+
+    @FXML
+    private Label triadLabel_1;
+    @FXML
+    private Label triadLabel_2;
+    @FXML
+    private Label triadLabel_3;
+
+    private RelatedColors relatedColors;
 
 
     public void initialize(URL location, ResourceBundle resources) {
@@ -71,21 +96,48 @@ public class TableController implements Initializable {
 
                 try {
                     Pixel pixel = pixels.get(((TableCell) event.getSource()).getIndex());
-                    RelatedColors relatedColors = new RelatedColors(pixel.getHexValue());
-                    
-                    Image compImage = new ThumbnailFromHexValue(relatedColors.getComplementary(),100,100).returnImage();
-                    compView = new ImageView(compImage);
-                    compLabel = new Label();
-                    compLabel.setText("Hex Value = " + relatedColors.getComplementary());
+                    relatedColors = new RelatedColors(pixel.getHexValue());
 
-                    box.getChildren().clear();
-                    box.getChildren().addAll(compLabel);
-                    box.getChildren().add(compView);
-                    compTab.setContent(box);
+                    ComplementaryColors();
+                    TriadColors();
 
                 } catch (IndexOutOfBoundsException e) {}
             }
         }
+    }
+
+    private void TriadColors() {
+        Image triadImage_1 = new ThumbnailFromHexValue(relatedColors.getTriad()[0],100,100).returnImage();
+        Image triadImage_2 = new ThumbnailFromHexValue(relatedColors.getTriad()[1],100,100).returnImage();
+        Image triadImage_3 = new ThumbnailFromHexValue(relatedColors.getTriad()[2],100,100).returnImage();
+
+        triadView_1 = new ImageView(triadImage_1);
+        triadView_2 = new ImageView(triadImage_2);
+        triadView_3 = new ImageView(triadImage_3);
+
+        triadLabel_1 = new Label();
+        triadLabel_2 = new Label();
+        triadLabel_3 = new Label();
+
+        triadLabel_1.setText("Hex Value = " + relatedColors.getTriad()[0]);
+        triadLabel_2.setText("Hex Value = " + relatedColors.getTriad()[1]);
+        triadLabel_3.setText("Hex Value = " + relatedColors.getTriad()[2]);
+
+        triadBox.getChildren().clear();
+        triadBox.getChildren().addAll(triadLabel_1,triadView_1,triadLabel_2,triadView_2,triadLabel_3,triadView_3);
+        triadTab.setContent(triadBox);
+    }
+
+    private void ComplementaryColors() {
+        Image compImage = new ThumbnailFromHexValue(relatedColors.getComplementary(),100,100).returnImage();
+        compView = new ImageView(compImage);
+        compLabel = new Label();
+        compLabel.setText("Hex Value = " + relatedColors.getComplementary());
+
+        compBox.getChildren().clear();
+        compBox.getChildren().addAll(compLabel);
+        compBox.getChildren().add(compView);
+        compTab.setContent(compBox);
     }
 }
 
